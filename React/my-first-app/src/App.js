fe5784532b6ba9981e150438fb857f106a0fd525
Ajay Bhosle <109852187+AjayBhosle17@users.noreply.github.com>
2024-02-19 01:30:40 +0530

    React-First-App

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="ajay">
+      
+      <h1>AJAY  BHOSLE</h1>
+      <h2>Hello I am a <span>Developer</span></h2>
+    </div>
+  );
+}
+
+export default App;
